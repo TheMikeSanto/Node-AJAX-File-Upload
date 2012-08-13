919abe679f465405196cec27679600266676f1e2
Mike Santo <mike.j.santo@gmail.com>
2012-08-13 16:41:47 -0500

    use serveFile to serve index.html and return a 405 if anything but a POST hits /description

--- server.js
+++ server.js
@@ -8,25 +8,15 @@ var http 				= require('http'),
 var server = http.createServer(function (req, res) {
 	// Serve up the main page containing the form
 	if (req.url === "/") {
+		serveFile(res, "./index.html", 'text/html');
     res.writeHead(200, "OK", {'Content-Type': 'text/html'});
-    fs.readFile('./index.html', function(err, html) {
-    	if (err) {
-    		throw err;
-    	} else { 
-    		res.end(html);
-  		}
-    });
   }
 
-  // Serve up the requested file to the user
-	if (req.url.split("/")[1] == "uploads") {
-		console.log("requesting file " + virtualToPhysical(req.url));
-		serveFile(res, virtualToPhysical(req.url), mime.lookup(req.url));
-	}
-
-	// Handle file uploads
+	// Handle file upload POSTs
 	if (req.url.split("?")[0] === "/upload") {
 		if (req.method.toLowerCase() === 'post') {
+			// Get the socket id out of the posted url
+			// and create a new form object for it
 			socket_id 				= req.url.split("sid=")[1];
 			forms[socket_id] 	= new formidable.IncomingForm();
 			form 							= forms[socket_id];
@@ -59,12 +49,20 @@ var server = http.createServer(function (req, res) {
 		}
 	}
 
-	// Handle description POSTS
+	// Handle description POSTs
 	if (req.url === "/description") {
 		if (req.method.toLowerCase() == 'post') {
 			res.writeHead(200, {"Content-Type": "text/html"});
 			res.end("success");
+		} else {
+			res.writeHead(405, "Method Not Allowed");
 		}
+	}
+
+  // Serve up the requested file to the user after upload
+	if (req.url.split("/")[1] == "uploads") {
+		console.log("requesting file " + virtualToPhysical(req.url));
+		serveFile(res, virtualToPhysical(req.url), mime.lookup(req.url));
 	}
 
 	// Handlers for loading static content
@@ -94,7 +92,7 @@ function serveFile(res, path, contentType) {
 		if (err) {
 			throw err;
 		} else {
-			console.log("serving static file " + path);
+			console.log("serving file " + path);
 			res.writeHead(200, {'Content-Type': contentType});
 			res.end(data);
 		}
